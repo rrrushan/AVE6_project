1cbb8eb9e27a339009e5878205dc01788f700f1f
Evgenia Louwe <evl4630@thi.de>
2023-05-07 14:00:38 +0200

    First attempt to calibrate

--- calibration/Carla/calibration_carla.py
+++ calibration/Carla/calibration_carla.py
@@ -2,30 +2,23 @@ import numpy as np
 import cv2 as cv
 import glob
 
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
 points_row = 10
 points_col = 7
 
 objp = np.zeros((points_row * points_col, 3), np.float32)
 objp[:,:2] = np.mgrid[0:points_row, 0:points_col].T.reshape(-1,2)
-objp *= 100
+objp *= 500
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-# # original images
-# images = glob.glob('Blender/01_Rendered_images/*.png')
-
-# # cropped images
-# images = glob.glob('Blender/02_Cropped/*.png')
-
-# # cropped and padded images
-# images = glob.glob('Blender/03_Cropped_Padded/*.png')
-
 # cropped, padded and resized images
-images = glob.glob('Blender/04_Resized/*.png')
+images = glob.glob('calibration/Carla/Calibration_Images/23_05_04/*.jpg')
 
-images.sort()
 for i, fname in enumerate(images):
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -35,7 +28,8 @@ for i, fname in enumerate(images):
    
     if ret == True:
         objpoints.append(objp)
-        imgpoints.append(corners)
+        corners2 = cv.cornerSubPix(gray,corners, (5,5), (-1,-1), criteria)
+        imgpoints.append(corners2)
 
         # Draw and display the corners
         cv.drawChessboardCorners(img, (points_row, points_col), corners, ret)
@@ -46,21 +40,16 @@ print('Image point arrays: ', len(imgpoints))
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 print(mtx)
 
-# def write_result(file_name, ret, mtx, dist, rvecs, tvecs, shape):
-#     with open(f'blender/{file_name}.txt', 'a') as f:
-#         f.write(f'Image shape:\n')
-#         f.write(f'{shape}\n')
-#         f.write(f'Error:\n')
-#         f.write(f'{ret}\n')
-#         f.write(f'Intrinsics:\n')
-#         f.write(f'{mtx}\n')
-#         f.write(f'Extrinsics:\n')
-#         for i in range(len(tvecs)):
-#             f.write(f'Image {i}:\n')
-#             f.write(f't:\n{tvecs[i]}\n')
-#             f.write(f'r:\n{rvecs[i]}\n')
-#         f.write(f'Distortion:\n')
-#         f.write(f'{dist}')
+def write_result(file_name, ret, mtx, dist, shape):
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(f'Image shape:\n')
+        f.write(f'{shape}\n')
+        f.write(f'Error:\n')
+        f.write(f'{ret}\n')
+        f.write(f'Intrinsics:\n')
+        f.write(f'{mtx}\n')
+        f.write(f'Distortion:\n')
+        f.write(f'{dist}')
 
-# filename = '23_04_27'
-# write_result(filename, ret, mtx, dist, rvecs, tvecs, img.shape)
+filename = 'calibration/Carla/07_05_23'
+write_result(filename, ret, mtx, dist, img.shape)
